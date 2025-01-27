8a33d9c6b16e8099d9765fa46e0cf544f23e20ae
Jinwoo Kim <kidjinwoo@me.com>
2025-01-28 02:41:25 +0900

    22C150

--- Sources/MySwiftUICore/Protobuf/ArchiveReader.swift
+++ Sources/MySwiftUICore/Protobuf/ArchiveReader.swift
@@ -1,5 +1,14 @@
 import Foundation
 
+/*
+ TODO:
+ FileArchiveReader
+ DataArchiveReader
+ ArchiveWriter
+ FileArchiveWriter
+ DataArchiveWrite
+ */
+
 package final class ArchiveReader {
     package static let readerKey = CodingUserInfoKey(rawValue: "com.apple.SwiftUI.ArchiveReader").unsafelyUnwrapped
     package static let cacheKey = CodingUserInfoKey(rawValue: "com.apple.SwiftUI.ArchiveReader.cache").unsafelyUnwrapped
@@ -8,14 +17,68 @@ package final class ArchiveReader {
     private let buffer: UnsafeBufferPointer<UInt8>
     private let attachments: UInt64
     
-    init(buffer: UnsafeBufferPointer<UInt8>) throws {
+    fileprivate init(buffer: UnsafeBufferPointer<UInt8>) throws {
+        let x0 = UnsafeRawPointer(buffer.baseAddress.unsafelyUnwrapped)
+        let x1 = UInt(buffer.count)
         
+        if (x1 <= 0) {
+            throw Error.invalidSize
+        }
+        
+        var x8 = x1 & 0x7
+        if x8 != 0 {
+            throw Error.invalidSize
+        }
+        
+        x8 = UInt(bitPattern: x0) + x1
+        var x9 = x8 - 0x4
+        
+        if x9 < UInt(bitPattern: x0) {
+            throw Error.invalidSize
+        }
+        
+        x9 = UnsafePointer<UInt>(bitPattern: x9)
+            .unsafelyUnwrapped
+            .pointee
+        
+        if x9 != 0xD1561A71 {
+            throw Error.invalidMagic
+        }
+        
+        var x10 = x8
+        let x11 = UnsafePointer<UInt>(bitPattern: x10 - 0x8)
+            .unsafelyUnwrapped
+            .pointee
+        
+        var x12 = x11 &<< 0x4
+        var x9_2 = Int(x12)
+        x12 = x10 - x12
+        
+        if x12 < UInt(bitPattern: x0) {
+            throw Error.invalidCount
+        }
+        
+        x10 = x12 - UInt(bitPattern: x0)
+        
+        fatalError("TODO")
     }
-    
+}
+
+extension ArchiveReader: RandomAccessCollection {
     package var startIndex: Int { buffer.startIndex }
     package var endIndex: Int { buffer.endIndex }
     
-    subscript(index: Int) -> Data {
-        
+    package subscript(index: Int) -> Data {
+        fatalError("TODO")
     }
+}
+
+fileprivate enum Error: Swift.Error {
+    case ioError(Int)
+    case unableToOpen
+    case invalidSize
+    case invalidMagic
+    case invalidCount
+    case invalidAttachment
+    case readFailed
 }
--- Sources/MySwiftUICore/Protobuf/ProtobufDecoder.swift
+++ Sources/MySwiftUICore/Protobuf/ProtobufDecoder.swift
@@ -3,6 +3,10 @@ import CoreText
 import _MySwiftUICShims
 
 package struct ProtobufDecoder {
+    package enum DecodingError: Swift.Error {
+        case failed
+    }
+    
     private var data: Data
     private var ptr: UnsafeRawPointer
     private var end: UnsafeRawPointer
@@ -12,7 +16,16 @@ package struct ProtobufDecoder {
     private var userInfo: [CodingUserInfoKey: Any]
     
     package init(_ data: Data) {
-        fatalError("TODO")
+        let nsData = data as NSData
+        let bytes = nsData.bytes
+        
+        self.data = data
+        ptr = bytes
+        end = bytes.advanced(by: data.count)
+        packedField = ProtobufFormat.Field(rawValue: 0)
+        packedEnd = bytes
+        stack = []
+        userInfo = [:]
     }
     
     package var preferredBundleLanguage: String? {
@@ -25,5 +38,20 @@ package struct ProtobufDecoder {
     
     package var archiveReader: ArchiveReader? {
         userInfo[ArchiveReader.readerKey] as? ArchiveReader
+    }
+    
+    package func value<Value>(fromBinaryPlist data: Data, type: Value.Type) throws -> Value where Value : Decodable {
+        let decoder = PropertyListDecoder()
+        let decoded = try decoder.decode([Value].self, from: data)
+        
+        guard let value = decoded.first else {
+            throw DecodingError.failed
+        }
+        
+        return value
+    }
+    
+    package func nextField() throws -> ProtobufFormat.Field? {
+        fatalError("TODO")
     }
 }
