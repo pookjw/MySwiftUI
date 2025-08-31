ef5246d12d980a26e1256766d3726e42eb7195f8
Jinwoo Kim <kidjinwoo@me.com>
2025-08-31 13:11:02 +0900

    no message

--- Sources/MySwiftUICoreTests/MovableLockTests.swift
+++ Sources/MySwiftUICoreTests/MovableLockTests.swift
@@ -3,72 +3,6 @@ private import _MySwiftUIShims
 private import Foundation
 private import _DarwinFoundation3.pthread
 
-fileprivate final class Box<U> {
-    let value: U
-    init(value: U) {
-        self.value = value
-    }
-}
-
-fileprivate func executeBlock<T>(_ block: @escaping () throws -> T) async throws -> T {
-    return try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<T, any Error>) in
-        executeBlock {
-            do {
-                let result = try block()
-                continuation.resume(returning: result)
-            } catch {
-                continuation.resume(throwing: error)
-            }
-        }
-    }
-}
-
-fileprivate func executeBlock(_ block: @escaping () -> Void) {
-    withUnsafeTemporaryAllocation(of: pthread_t.self, capacity: 1) { threadPointer in
-        let box = Box(value: block)
-        let castedThreadPointer = UnsafeMutableRawPointer(threadPointer.baseAddress.unsafelyUnwrapped)
-            .assumingMemoryBound(to: pthread_t?.self)
-        
-        pthread_create(
-            castedThreadPointer,
-            nil,
-            { boxPointer in
-                return autoreleasepool {
-                    let box = unsafeBitCast(boxPointer, to: Box<() -> Void>.self)
-                    box.value()
-                    Unmanaged.passUnretained(box).release()
-                    return nil
-                }
-            },
-            Unmanaged.passRetained(box).toOpaque()
-        )
-        
-        pthread_detach(castedThreadPointer.pointee.unsafelyUnwrapped)
-    }
-}
-
-fileprivate func executeBlock<T>(_ block: @escaping () -> T) async -> T {
-    return await withCheckedContinuation { (continuation: CheckedContinuation<T, Never>) in
-        executeBlock {
-            let result = block()
-            continuation.resume(returning: result)
-        }
-    }
-}
-
-fileprivate final class MyExecutor: TaskExecutor {
-    func asUnownedTaskExecutor() -> UnownedTaskExecutor {
-        return UnownedTaskExecutor(self)
-    }
-    
-    func enqueue(_ job: UnownedJob) {
-        executeBlock { 
-            job.runSynchronously(on: self.asUnownedTaskExecutor())
-        }
-    }
-}
-
-
 struct MovableLockTests {
     @Test
     func test_createAndDestory() {
@@ -76,8 +10,8 @@ struct MovableLockTests {
         lock.destory()
     }
     
-    @Test
-    func test_lockAndUnlock() async {
+    @Test(arguments: 0..<100)
+    func test_lockAndUnlock(_: Int) async {
         actor Counter {
             var number = 0
             func increment() {
@@ -89,18 +23,20 @@ struct MovableLockTests {
         let lock = MovableLock.create()
         let (stream, continuiation) = AsyncStream<Void>.makeStream()
         
-        let task = Task(executorPreference: MyExecutor()){
+        let task = Task(executorPreference: RunLoopTaskExecutor()){
             lock.lock()
             await Task.yield()
             continuiation.yield()
             await counter.increment()
             let result = await counter.number == 1
+            print("1")
             lock.unlock()
             return result
         }
         
         for await _ in stream { break }
         lock.lock()
+        print("2")
         
         await counter.increment()
         let number = await counter.number
@@ -112,8 +48,8 @@ struct MovableLockTests {
         lock.destory()
     }
     
-    @Test
-    func test_isOwner() async {
+    @Test(arguments: 0..<100)
+    func test_isOwner(_: Int) async {
         let lock = MovableLock.create()
         
         lock.lock()
---
+++ Sources/MySwiftUICoreTests/ThreadUtils.swift
@@ -0,0 +1,190 @@
+private import CoreFoundation
+private import _DarwinFoundation3.pthread
+private import os.lock
+
+final class RunLoopTaskExecutor: TaskExecutor {
+    private final class Storage: @unchecked Sendable {
+        let lock = OSAllocatedUnfairLock()
+        var runLoop: CFRunLoop?
+        var source: CFRunLoopSource?
+        var jobs: [UnownedJob] = []
+        var needsToStop = false
+        var executor: UnownedTaskExecutor?
+    }
+    
+    private nonisolated(unsafe) var thread: pthread_t?
+    private let storage = Storage()
+    
+    init() {
+        storage.executor = asUnownedTaskExecutor()
+        
+        pthread_create(
+            &thread,
+            nil,
+            { context in
+                return autoreleasepool {
+                    let storage = Unmanaged<Storage>.fromOpaque(context)
+                    defer {
+                        storage.release()
+                    }
+                    
+                    var context = CFRunLoopSourceContext(
+                        version: 0,
+                        info: storage.toOpaque(),
+                        retain: { ref in
+                            return UnsafeRawPointer(Unmanaged<Storage>.fromOpaque(ref.unsafelyUnwrapped).retain().toOpaque())
+                        },
+                        release: { ref in
+                            Unmanaged<Storage>.fromOpaque(ref.unsafelyUnwrapped).release()
+                        },
+                        copyDescription: nil,
+                        equal: nil,
+                        hash: nil,
+                        schedule: nil,
+                        cancel: nil,
+                        perform: { ref in
+                            let storage = Unmanaged<Storage>.fromOpaque(ref.unsafelyUnwrapped)
+                            storage.takeUnretainedValue().lock.withLockUnchecked {
+                                for job in storage.takeUnretainedValue().jobs {
+                                    autoreleasepool {
+                                        job.runSynchronously(on: storage.takeUnretainedValue().executor.unsafelyUnwrapped)
+                                    }
+                                }
+                                storage.takeUnretainedValue().jobs.removeAll()
+                            }
+                        }
+                    )
+                    
+                    let source = CFRunLoopSourceCreate(kCFAllocatorDefault, 0, &context)
+                    let runLoop = CFRunLoopGetCurrent()
+                    CFRunLoopAddSource(runLoop, source, .defaultMode)
+                    
+                    let needToStop = storage.takeUnretainedValue().lock.withLockUnchecked {
+                        storage.takeUnretainedValue().source = source
+                        storage.takeUnretainedValue().runLoop = runLoop
+                        
+                        guard !storage.takeUnretainedValue().needsToStop else {
+                            return true
+                        }
+                        
+                        for job in storage.takeUnretainedValue().jobs {
+                            autoreleasepool {
+                                job.runSynchronously(on: storage.takeUnretainedValue().executor.unsafelyUnwrapped)
+                            }
+                        }
+                        
+                        storage.takeUnretainedValue().jobs.removeAll()
+                        return false
+                    }
+                    
+                    guard !needToStop else {
+                        return nil
+                    }
+                    
+                    CFRunLoopRun()
+                    return nil
+                }
+            },
+            Unmanaged.passRetained(storage).toOpaque()
+        )
+        
+        pthread_detach(thread.unsafelyUnwrapped)
+    }
+    
+    deinit {
+        if pthread_self() == thread {
+            storage.needsToStop = true
+            if let runLoop = storage.runLoop {
+                CFRunLoopStop(runLoop)
+            }
+            if let source = storage.source {
+                CFRunLoopSourceInvalidate(source)
+            }
+        } else {
+            storage.lock.withLockUnchecked {
+                storage.needsToStop = true
+                if let runLoop = storage.runLoop {
+                    while !CFRunLoopIsWaiting(runLoop) {}
+                    CFRunLoopStop(runLoop)
+                }
+                if let source = storage.source {
+                    CFRunLoopSourceInvalidate(source)
+                }
+            }
+        }
+    }
+    
+    func asUnownedTaskExecutor() -> UnownedTaskExecutor {
+        return UnownedTaskExecutor(self)
+    }
+    
+    func enqueue(_ job: consuming ExecutorJob) {
+        if pthread_self() == thread {
+            job.runSynchronously(on: asUnownedTaskExecutor())
+        } else {
+            let unownedJob = UnownedJob(job)
+            storage.jobs.append(unownedJob)
+            
+            if let source = storage.source {
+                CFRunLoopSourceSignal(source)
+            }
+            if let runLoop = storage.runLoop {
+                CFRunLoopWakeUp(runLoop)
+            }
+        }
+    }
+}
+
+
+fileprivate final class Box<U> {
+    let value: U
+    init(value: U) {
+        self.value = value
+    }
+}
+
+func executeBlock<T>(_ block: @escaping () throws -> T) async throws -> T {
+    return try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<T, any Error>) in
+        executeBlock {
+            do {
+                let result = try block()
+                continuation.resume(returning: result)
+            } catch {
+                continuation.resume(throwing: error)
+            }
+        }
+    }
+}
+
+func executeBlock(_ block: @escaping () -> Void) {
+    withUnsafeTemporaryAllocation(of: pthread_t.self, capacity: 1) { threadPointer in
+        let box = Box(value: block)
+        let castedThreadPointer = UnsafeMutableRawPointer(threadPointer.baseAddress.unsafelyUnwrapped)
+            .assumingMemoryBound(to: pthread_t?.self)
+        
+        pthread_create(
+            castedThreadPointer,
+            nil,
+            { boxPointer in
+                return autoreleasepool {
+                    let box = unsafeBitCast(boxPointer, to: Box<() -> Void>.self)
+                    box.value()
+                    Unmanaged.passUnretained(box).release()
+                    return nil
+                }
+            },
+            Unmanaged.passRetained(box).toOpaque()
+        )
+        
+        pthread_detach(castedThreadPointer.pointee.unsafelyUnwrapped)
+    }
+}
+
+func executeBlock<T>(_ block: @escaping () -> T) async -> T {
+    return await withCheckedContinuation { (continuation: CheckedContinuation<T, Never>) in
+        executeBlock {
+            let result = block()
+            continuation.resume(returning: result)
+        }
+    }
+}
