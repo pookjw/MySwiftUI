203ea7c2e1440adec5aaf775aecb86cdc5cf0536
Jinwoo Kim <kidjinwoo@me.com>
2025-10-29 19:40:26 +0900

    no message

--- Sources/MySwiftUICore/RemoveGlobalActorIsolation.swift
+++ Sources/MySwiftUICore/RemoveGlobalActorIsolation.swift
@@ -1,7 +1,7 @@
 // E36588D6F4797F7C6EF26CC7E1C2D1CE
 
 @globalActor public actor _ImpossibleActor {
-    public static nonisolated let shared = _ImpossibleActor()
+    public static let shared = _ImpossibleActor()
     
     public nonisolated var unownedExecutor: UnownedSerialExecutor {
         return _ImpossibleExecutor.shared.asUnownedSerialExecutor()
