694a19c189eb445f8a2b23160a1059ca0dd8c5ad
Jinwoo Kim <kidjinwoo@me.com>
2026-02-03 22:09:07 +0900

    no message

--- Sources/MySwiftUICore/EnvironmentKeyWritingModifier.swift
+++ Sources/MySwiftUICore/EnvironmentKeyWritingModifier.swift
@@ -1,4 +1,4 @@
-// 3B04936F6043A290A3E53AE94FE09355
+// 3B04936F6043A290A3E53AE94FE09355, E370275CDB55AC7AD9ACF0420859A9E8
 private import AttributeGraph
 
 extension View {
@@ -46,14 +46,102 @@ fileprivate struct ChildEnvironment<Value>: StatefulRule, AsyncAttribute, Custom
     
     typealias Value = EnvironmentValues
     
-    func updateValue() {
+    mutating func updateValue() {
         // x24(sp), x21 -> x19 -> (sp + sizeof(Value)
         let (modifier, modifierChanged) = $modifier.changedValue(options: [])
         // self -> sp + 0x78
         // x21, x23 -> sp + 0xa0, sp + 0xb0
-        let (env, envChanged) = $env.changedValue(options: [])
+        var (env, envChanged) = $env.changedValue(options: [])
         
-        fatalError("TODO")
+        if !envChanged {
+            if modifierChanged {
+                // <+844>
+                let result: Bool? = oldModifier.map { oldModifier in
+                    // $s7SwiftUI16ChildEnvironment33_3B04936F6043A290A3E53AE94FE09355LLV11updateValueyyFSbyKXEfu_SbyKXEfu0_SbAA01_D18KeyWritingModifierVyxGXEfU_
+                    /*
+                     oldModifier -> x0 -> x19
+                     modifier -> x1 -> x20
+                     Value -> x2 -> x23
+                     */
+                    // x25 -> sp + 0x68
+                    let oldKeyPath = oldModifier.keyPath
+                    // x28(sp)
+                    let copy_1 = modifier
+                    // x19
+                    let newKeyPath = copy_1.keyPath
+                    _ = consume copy_1
+                    // newKeyPath -> sp + 0x58
+                    // x20
+                    let result = oldKeyPath == newKeyPath
+                    
+                    guard result else {
+                        return true
+                    }
+                    
+                    // <+340>
+                    // x28
+                    let copy_2 = modifier
+                    // x25
+                    let newValue = copy_2.value
+                    _ = consume copy_2
+                    
+                    return !compareValues(oldModifier.value, newValue, options: [.unknown1])
+                }
+                
+                if let result {
+                    if result {
+                        // <+448>
+                    } else {
+                        // <+392>
+                        if hasValue {
+                            // <+408>
+                            // <+772>
+                            return
+                        } else {
+                            // <+448>
+                        }
+                    }
+                } else {
+                    // <+448>
+                }
+            } else {
+                // <+376>
+                if hasValue {
+                    // <+408>
+                    // <+772>
+                    return
+                } else {
+                    // <+448>
+                }
+            }
+        } else {
+            // <+432>
+            // <+448>
+        }
+        
+        // <+448>
+        // x25
+        let copy_1 = modifier
+        // modifier -> sp + 0x80
+        // x21
+        let newKeyPath = copy_1.keyPath
+        _ = consume copy_1
+        
+        // <+508>
+        // x26
+        let copy_2 = modifier
+        // x27
+        let newValue = copy_2.value
+        _ = consume copy_2
+        
+        // <+564>
+        env[keyPath: newKeyPath] = newValue
+        self.value = env
+        
+        // self -> x23
+        // x25
+        let copy_3 = modifier
+        self.oldModifier = modifier
     }
     
     var description: String {
