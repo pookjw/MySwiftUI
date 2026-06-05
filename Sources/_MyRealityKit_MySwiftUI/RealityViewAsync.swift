d9a6550067b9ca4c4112f3bb153164eb1117a40e
Jinwoo Kim <kidjinwoo@me.com>
2026-06-05 20:57:42 +0900

    no message

---
+++ Sources/_MyRealityKit_MySwiftUI/RealityViewAsync.swift
@@ -0,0 +1,20 @@
+//internal import MySwiftUICore
+//private import MyRealityFoundation
+//private import CoreGraphics
+//private import UIKit
+//private import Combine
+
+struct _RealityViewAsync<Placeholder> {
+//    let make: @Sendable (inout RealityViewContent) async -> ()
+//    let update: (@MainActor (inout RealityViewContent) -> ())?
+//    let placeholder: Placeholder
+//    let proxy: GeometryProxy3D
+//    let controller: AttachmentStateControllerBase?
+//    weak var model: _RealityViewModel?
+//    var _scene: Environment<Scene?>
+//    var _pointsPerMeter: Environment<CGFloat>
+//    var _role: Environment<UISceneSession.Role?>
+//    var _uiScene: Environment<UIScene?>
+//    var _layoutOption: Environment<SwiftViewLayoutOption>
+//    var _componentAddedSubscription: State<any Cancellable?>
+}
