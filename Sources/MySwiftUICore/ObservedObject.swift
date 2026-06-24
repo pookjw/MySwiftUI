8d7921020e90c5a78baa70c37bf5640726beb9fd
Jinwoo Kim <kidjinwoo@me.com>
2026-06-25 03:16:10 +0900

    no message

--- Sources/MySwiftUICore/ObservedObject.swift
+++ Sources/MySwiftUICore/ObservedObject.swift
@@ -1,6 +1,8 @@
 // C212C242BFEB175E53A59438AB276A7C
 public import Combine
 internal import AttributeGraph
+private import _DarwinFoundation3.pthread
+private import os.log
 
 @propertyWrapper @preconcurrency @MainActor @frozen public struct ObservedObject<ObjectType> : DynamicProperty where ObjectType : ObservableObject {
     @dynamicMemberLookup @preconcurrency @MainActor @frozen public struct Wrapper {
@@ -12,6 +14,7 @@ internal import AttributeGraph
     
     @usableFromInline
     @MainActor @preconcurrency internal var _seed: Int = 0
+    
     @_alwaysEmitIntoClient @MainActor @preconcurrency public init(initialValue: ObjectType) {
         self.init(wrappedValue: initialValue)
     }
@@ -55,16 +58,39 @@ extension ObservedObject {
 }
 
 fileprivate struct ObservedObjectPropertyBox<ObjectType : ObservableObject> : DynamicPropertyBox {
-    private let subscriber: AttributeInvalidatingSubscriber<ObservableObjectPublisher>
-    private let lifetime = SubscriptionLifetime<ObservableObjectPublisher>()
-    private var seed: Int = 0
-    private var lastObject: ObjectType? = nil
+    private let subscriber: AttributeInvalidatingSubscriber<ObservableObjectPublisher> // 0x0
+    private let lifetime = SubscriptionLifetime<ObservableObjectPublisher>() // 0x8
+    private var seed: Int = 0 // 0x10
+    private var lastObject: ObjectType? = nil // 0x18
     
     init(host: GraphHost, invalidation: WeakAttribute<Void>) {
         self.subscriber = AttributeInvalidatingSubscriber<ObservableObjectPublisher>(host: host, attribute: invalidation)
     }
     
-    func update(property: inout ObservedObject<ObjectType>, phase: _GraphInputs.Phase) -> Bool {
-        assertUnimplemented()
+    mutating func update(property: inout ObservedObject<ObjectType>, phase: _GraphInputs.Phase) -> Bool {
+        if pthread_main_np() == 0 {
+            unsafe os_log(.fault, log: .runtimeIssuesLog, "Updating ObservedObject<\(_typeName(ObjectType.self, qualified: false))> from background threads will cause undefined behavior; make sure to update it from the main thread.")
+        }
+        
+        // <+296>
+        MainActor.assumeIsolated { 
+            // $s7SwiftUI25ObservedObjectPropertyBox33_C212C242BFEB175E53A59438AB276A7CLLV6update8property5phaseSbAA0cD0VyxGz_AA12_GraphInputsV5PhaseVtFyyScMYcXEfU_
+            /*
+             property -> x0 -> x19
+             self -> x1 -> x22
+             */
+            assertUnimplemented()
+        }
+        
+        let changed = self.subscriber.attribute.changedValue(options: [])?.changed ?? false
+        
+        var seed = self.seed
+        if changed {
+            seed &+= 1
+            self.seed = seed
+        }
+        property._seed = seed
+        
+        return changed
     }
 }
