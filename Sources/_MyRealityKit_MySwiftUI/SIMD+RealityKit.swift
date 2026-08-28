4bfdce7db53987742e18e786c470d4f0449605d4
Jinwoo Kim <kidjinwoo@me.com>
2026-08-28 19:41:32 +0900

    no message

--- Sources/_MyRealityKit_MySwiftUI/SIMD+RealityKit.swift
+++ Sources/_MyRealityKit_MySwiftUI/SIMD+RealityKit.swift
@@ -2,6 +2,13 @@ internal import simd
 
 extension simd_float4x4 {
     init(translation: SIMD3<Float>) {
-        assertUnimplemented()
+        let rows: [SIMD4<Float>] = [
+            SIMD4<Float>(1, 0, 0, translation.x),
+            SIMD4<Float>(0, 1, 0, translation.y),
+            SIMD4<Float>(0, 0, 1, translation.z),
+            SIMD4<Float>(0, 0, 0, 1)
+        ]
+        
+        self.init(rows: rows)
     }
 }
