afb818a08c23f2d0207f294c798af8b25af810b4
Jinwoo Kim <kidjinwoo@me.com>
2026-09-21 18:12:30 +0900

    no message

--- Sources/MySwiftUICore/SecondaryLayerGeometry.swift
+++ Sources/MySwiftUICore/SecondaryLayerGeometry.swift
@@ -114,6 +114,8 @@ fileprivate func makeSecondaryPreferenceView<T : PreferenceKey, U : View>(
     var copy_2 = inputs
     // x19 + 0x1c0 (sp + 0x1c0)
     let copy_3 = copy_1
+    // x28 + 0x60 (sp + 0x160)
+    let copy_4 = inputs.base
     
     // <+188>
     copy_2.preferences.add(T.self)
@@ -126,9 +128,9 @@ fileprivate func makeSecondaryPreferenceView<T : PreferenceKey, U : View>(
     // x19 + 0xc (sp + 0xc)
     let position = copy_1.position
     // x19 + 0x1c0 (sp + 0x1c0)
-    let outputs = body(_Graph(), copy_2)
+    let outputs_1 = body(_Graph(), copy_2)
     // x19 + 0x58 (sp + 0x58)
-    let copy_4 = outputs
+    let copy_5 = outputs_1
     
     let query = Attribute(
         SecondaryLayerGeometryQuery(
@@ -136,36 +138,81 @@ fileprivate func makeSecondaryPreferenceView<T : PreferenceKey, U : View>(
             layoutDirection: copy_1.layoutDirection,
             primaryPosition: position,
             primarySize: size,
-            primaryLayoutComputer: OptionalAttribute(copy_4.layoutComputer),
+            primaryLayoutComputer: OptionalAttribute(copy_5.layoutComputer),
             secondaryLayoutComputer: OptionalAttribute()
         )
     )
     
     // <+516>
     // x28 (sp + 0x100)
-    var copy_5 = copy_1
+    var copy_6 = copy_1
     // x19 + 0x1c0 (sp + 0x1c0)
-    let copy_6 = copy_1
+    let copy_7 = copy_1
     
     // <+564>
-    copy_5.position = query.origin
-    copy_5.size = query.dimensions.size
+    copy_6.position = query.origin
+    copy_6.size = query.dimensions.size
     
     // <+632>
-    copy_5.implicitRootType = _ZStackLayout.self
-    copy_5.base.pushStableIndex(1)
+    copy_6.implicitRootType = _ZStackLayout.self
+    copy_6.base.pushStableIndex(1)
     
     // <+692>
     // x19 + 0x78 (sp + 0x78)
-    let copy_7 = outputs
-//    copy_7[T.self]
-//    
-//    _ViewOutputs.makeDepthTransform(
-//        inputs: <#T##_ViewInputs#>,
-//        geometry: <#T##() -> Attribute<ViewDepthGeometry>#>,
-//        body: <#T##(_ViewInputs) -> _ViewOutputs#>
-//    )
+    let copy_8 = outputs_1
+    let value = copy_8[T.self]
     
+    let newBody: (_Graph, _ViewInputs) -> _ViewOutputs = { _, incoming in
+        // $s7SwiftUI27makeSecondaryPreferenceView33_E16FB36220E60E9D0940B6710E02DAD2LL8modifier6inputs4body9flipOrderAA01_F7OutputsV14AttributeGraph0U0VyAA08_OverlayE8ModifierVyxq_GG_AA01_F6InputsVAiA01_V0V_ARtcSbtAA0E3KeyRzAA0F0R_r0_lFAiT_ARtcfU0_
+        assertUnimplemented()
+    }
+    
+    // x19 + 0xa0 (sp + 0xa0)
+    let copy_9 = copy_4
+    // x19 + 0x1c0 (sp + 0x1c0)
+    let copy_10 = copy_4
+    
+    // x19 + 0x68 (sp + 0xc8)
+    let outputs_2: _ViewOutputs
+    
+    if IsVisionEnabledPredicate.evaluate(inputs: copy_9) {
+        // <+880>
+        var x190x9c: Attribute<ViewTransform>? = nil
+        
+        // x19 + 0xa0 (sp + 0xa0)
+        let copy_11 = copy_6
+        
+        outputs_2 = _ViewOutputs.makeDepthTransform(
+            inputs: copy_11,
+            geometry: {
+                // $s7SwiftUI25makePlatformSecondaryView13primaryInputs0G7Outputs09secondaryH09flipOrder4bodyAA01_fI0VAA01_fH0V_AiKSbAiA6_GraphV_AKtctF09AttributeN00O0VyAA0F13DepthGeometryVGyXEfU_TA.19
+                assertUnimplemented()
+            },
+            body: { inputs in
+                // $s7SwiftUI27ViewModifierContentProvider33_2BA0A33A15B7F322F46AFB9D0D1A262DLLPAAE012providerMakeC4List4view6inputsAA01_cS7OutputsVAA11_GraphValueVyxG_AA01_cS6InputsVtFZAA01_cV0VAA01_cY0VcfU_TA.110
+                return newBody(_Graph(), inputs)
+            }
+        )
+        
+        if let x190x9c {
+            // <+1196>
+            x190x9c.mutateBody(as: RootDepthTransform.self, invalidating: true) { transform in
+                transform.$childLayoutComputer = outputs_2.layoutComputer
+            }
+            
+            // <+1316>
+        } else {
+            // <+1316>
+        }
+    } else {
+        // <+1100>
+        // x19 + 0xa0 (sp + 0xa0)
+        let copy_11 = copy_6
+        outputs_2 = newBody(_Graph(), copy_11)
+        // <+1316>
+    }
+    
+    // <+1320>
     assertUnimplemented()
 }
 
